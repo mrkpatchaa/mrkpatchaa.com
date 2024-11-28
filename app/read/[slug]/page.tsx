91d767ce27f939003d413c77902d15e40a88f9a3
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2024-11-28 14:36:31 +0100

    fix: slugs typo

--- app/read/[slug]/page.tsx
+++ app/read/[slug]/page.tsx
@@ -43,7 +43,7 @@ export async function generateMetadata(
   }
 }
 
-export default async function Post(props: { params: Promise<{ slugs: string[] }> }) {
+export default async function Post(props: { params: Promise<{ slug: string }> }) {
   const {
     params: { slug },
   } = await props.params
